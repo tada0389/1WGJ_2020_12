cb3e764c3d42a4df7707e59b47379a18521bb468
Zakky03 <zakky0317@icloud.com>
2020-12-26 18:11:05 +0900

    にじりよりスケルトン追加

---
+++ Assets/Zakky/Skelton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Skelton : MonoBehaviour
+{
+    [SerializeField]
+    Transform mCameraTrans;
+    [SerializeField]
+    float mMovingTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //vec = mCameraTrans.position - transform.position;
+        Vector3 vec = mCameraTrans.position;
+        vec.y = transform.position.y;
+        var look = Quaternion.LookRotation(vec);
+        transform.localRotation = look;
+        transform.DOMove(vec,mMovingTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += vec * (Time.deltaTime / mMovingTime);
+    }
+}
