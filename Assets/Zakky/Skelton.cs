5178c9ddee3ef3905a80a2b5fb4b7146766716ff
Zakky03 <zakky0317@icloud.com>
2020-12-27 14:14:17 +0900

    Skeleton大体できた

--- Assets/Zakky/Skelton.cs
+++ Assets/Zakky/Skelton.cs
@@ -9,20 +9,27 @@ public class Skelton : MonoBehaviour
     Transform mCameraTrans;
     [SerializeField]
     float mMovingTime;
+    [SerializeField]
+    float mRunningSpeed = 5f;
+    [SerializeField]
+    AudioClip[] mGetaSFX;
 
-    enum SkeltonState
+    float mDisTanceToCamera;
+    Vector3 IniPos;
+
+    enum SkeletonState
     {
         Walk,
-        Run,
-        Dead
+        Run
     }
 
-    [SerializeField]
-    SkeltonState mSkeltonState = SkeltonState.Walk;
+    SkeletonState mSkeletonState;
 
-    ZakkyLib.Timer[] mTimer = new ZakkyLib.Timer[System.Enum.GetNames(typeof(SkeltonState)).Length];
+    ZakkyLib.Timer[] mTimer = new ZakkyLib.Timer[System.Enum.GetNames(typeof(SkeletonState)).Length];
+    ZakkyLib.Timer mGetaTimer;
 
     Animator mAnimator;
+    AudioSource mAudioSource;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,45 +37,91 @@ public class Skelton : MonoBehaviour
         vec.y = transform.position.y;
         var look = Quaternion.LookRotation(vec - transform.position);
         transform.localRotation = look;
-        transform.DOMove(vec,mMovingTime);
+        //transform.DOMove(vec,mMovingTime);
+        IniPos = transform.position;
 
-        mTimer[(int)SkeltonState.Walk] = new ZakkyLib.Timer(3f);
-        mTimer[(int)SkeltonState.Run] = new ZakkyLib.Timer(2f);
+        mDisTanceToCamera = (vec - transform.position).magnitude;
+
+        mSkeletonState = SkeletonState.Walk;
+        mTimer[(int)SkeletonState.Walk] = new ZakkyLib.Timer(3f + Random.Range(0f, 5f));
+        mTimer[(int)SkeletonState.Run] = new ZakkyLib.Timer(99999f);
+
+        mGetaTimer = new ZakkyLib.Timer(1f / SpeedCoff());
 
         mAnimator = GetComponent<Animator>();
+        mAudioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetWalkState();
+        }
+
         StateChange();
 
         SetStateIntoAnimator();
+
+        SkeletonMove();
+
+        if (mGetaTimer.IsTimeout())
+        {
+            mAudioSource.PlayOneShot(mGetaSFX[Random.Range(0, mGetaSFX.Length)]);
+            mGetaTimer.TimeReset();
+        }
+    }
+
+    float SpeedCoff()
+    {
+        if (mSkeletonState == SkeletonState.Run) return Mathf.Max(1f, mRunningSpeed);
+        if (mSkeletonState == SkeletonState.Walk) return 1f;
+        return 0f;
     }
 
     void StateChange()
     {
-        if (mSkeltonState == SkeltonState.Walk && mTimer[(int)SkeltonState.Walk].IsTimeout())
+        if (mSkeletonState == SkeletonState.Walk && mTimer[(int)SkeletonState.Walk].IsTimeout())
         {
-            mSkeltonState = SkeltonState.Run;
-            var t = mTimer[(int)SkeltonState.Run];
-            t = new ZakkyLib.Timer(t.GetLimitTime());
+            mSkeletonState = SkeletonState.Run;
+            mTimer[(int)SkeletonState.Run].TimeReset();
+            mGetaTimer = new ZakkyLib.Timer(1f / SpeedCoff());
         }
-        else if (mSkeltonState == SkeltonState.Run && mTimer[(int)SkeltonState.Run].IsTimeout())
+        else if (mSkeletonState == SkeletonState.Run && mTimer[(int)SkeletonState.Run].IsTimeout())
         {
-            mSkeltonState = SkeltonState.Walk;
-            var t = mTimer[(int)SkeltonState.Walk];
-            t = new ZakkyLib.Timer(t.GetLimitTime());
+            mSkeletonState = SkeletonState.Walk;
+            mTimer[(int)SkeletonState.Walk].TimeReset();
+            mGetaTimer = new ZakkyLib.Timer(1f / SpeedCoff());
         }
     }
 
     void SetStateIntoAnimator()
     {
-        mAnimator.SetBool("IsWalking", mSkeltonState == SkeltonState.Walk);
-        mAnimator.SetBool("IsDead", mSkeltonState == SkeltonState.Dead);
+        mAnimator.SetBool("IsWalking", mSkeletonState == SkeletonState.Walk);
+        //mAnimator.SetBool("IsDead", mSkeltonState == SkeltonState.Dead);
     }
 
-    public void DaedSetter()
+    private void SkeletonMove()
     {
-        mSkeltonState = SkeltonState.Dead;
+        Vector3 vec = mCameraTrans.position - transform.position;
+        vec.y = 0f;
+        transform.position += vec.normalized * mDisTanceToCamera * SpeedCoff() * (Time.deltaTime / mMovingTime);
     }
+
+    public void SetWalkState()
+    {
+        mSkeletonState = SkeletonState.Walk;
+        mTimer[(int)SkeletonState.Walk].TimeReset();
+        mGetaTimer = new ZakkyLib.Timer(1f / SpeedCoff());
+    }
+
+    public void SkeltonPosReset()
+    {
+        transform.position = IniPos;
+    }
+
+    //public void DaedSetter()
+    //{
+    //    mSkeltonState = SkeltonState.Dead;
+    //}
 }
