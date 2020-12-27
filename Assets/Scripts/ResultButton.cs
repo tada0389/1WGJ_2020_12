b24851f718140842359dd87158f45465cbcd9e0c
tada <tatata0389@gmail.com>
2020-12-27 13:14:20 +0900

    MainGameでギャラリー表示、タイトルへ戻るボタン

---
+++ Assets/Scripts/ResultButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultButton : MonoBehaviour
+{ 
+    public void OnRankingButtonClicked()
+    {
+        naichilab.RankingLoader.Instance.SendScoreAndShowRanking(ClearCheckController.curScore_);
+    }
+
+    public void OnHomeButtonClicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+    }
+}
