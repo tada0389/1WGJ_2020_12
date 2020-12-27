be2a23343520682a1a653abad682fa9948221fba
tada <tatata0389@gmail.com>
2020-12-27 17:53:18 +0900

    GameOverシーンでシーン遷移、暗転処理

---
+++ Assets/Scripts/GameOver/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField]
+    private float sceneTransitionTime_ = 2.0f;
+
+    [SerializeField]
+    private UnityEngine.UI.Image antenImage_;
+
+    // Start is called before the first frame update
+    private IEnumerator Start()
+    {
+
+        yield return new WaitForSeconds(sceneTransitionTime_ / 2.0f);
+
+        antenImage_.DOFade(1.0f, sceneTransitionTime_ / 2.0f);
+
+        yield return new WaitForSeconds(sceneTransitionTime_ / 2.0f);
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+    }
+}
