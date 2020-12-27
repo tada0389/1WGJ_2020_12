8c097f9e28e096d2c252963dc855a633cca1d798
tada <tatata0389@gmail.com>
2020-12-27 16:39:36 +0900

    MainGameでのカメラ処理、スケルトン処理

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -27,15 +27,38 @@ public class CameraController : MonoBehaviour
     private Skelton skeltonCtrl_;
 
     private float time_;
+    private bool lock_ = false;
 
     // Start is called before the first frame update
     void Start()
     {
         curDir_ = eDir.Forward;
+        time_ = Time.time - 10.0f;
+    }
+
+    public void LookForward()
+    {
+        lock_ = true;
+
+        float prevDif = Time.time - time_;
+        float newDuration = duration_ - Mathf.Max(0f, duration_ - prevDif);
+
+        if (curDir_ == eDir.Backward)
+        {
+            curDir_ = eDir.Forward;
+
+            // 後ろを見る
+            transform.DOKill();
+            transform.DORotate(new Vector3(0f, 0f, 0f), newDuration).SetEase(turnEase_);
+
+            lookText_.text = "BACK";
+        }
     }
 
     public void OnTurnAroundButtonClicked()
     {
+        if (lock_) return;
+
         float prevDif = Time.time - time_;
         float newDuration = duration_ - Mathf.Max(0f, duration_ - prevDif);
 
